d78e29283ca69a0dfb8c6035af4c80eabea39a0a
KastureeK <45508513+KastureeK@users.noreply.github.com>
2021-10-23 14:45:41 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='music',
+    version='',
+    packages=['gui', 'Controls', 'database'],
+    url='',
+    license='',
+    author='Kasturee K',
+    author_email='kastureekorgaonkar@gmail.com',
+    description='Mood Based Music Recommendation System'
+)
